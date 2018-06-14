e2a52faa2dcbaad6db7f4b81b689990b2ddb5b3d
Rebekah Sherman <dirtbeans@gmail.com>
2018-06-14 14:22:37 -0400

    skeleton of UI

--- c#-mini-capstone/Capstone/Classes/UserInterface.cs
+++ c#-mini-capstone/Capstone/Classes/UserInterface.cs
@@ -20,8 +20,52 @@ namespace Capstone.Classes
             bool done = false;
             while (!done)
             {
-                Console.WriteLine("This is the UserInterface");
-                Console.ReadLine();
+                Console.WriteLine("{1} Display Vending Machine Items");
+                Console.WriteLine("{2} Purchase");
+                Console.WriteLine("[3] Exit");
+                string mainMenuSelectionString = Console.ReadLine();
+                int mainMenuSelection = Convert.ToInt32(mainMenuSelectionString);
+
+                if ( mainMenuSelection == 1)
+                {
+                    //display Inventory
+                    Console.WriteLine();
+                }
+                else if (mainMenuSelection == 2)
+                {
+                    Console.WriteLine("{1} Feed Money");
+                    Console.WriteLine("{2} Select Product");
+                    Console.WriteLine("[3] Finish Transation");
+                    // Console.WriteLine(balance);
+                    string purchaseMenuSelectionString = Console.ReadLine();
+                    int purchaseMenuSelection = Convert.ToInt32(purchaseMenuSelectionString);
+
+                    if (purchaseMenuSelection==1)
+                    {
+                        //balance
+                    }
+                    else if (purchaseMenuSelection==2)
+                    {
+                        //call select product method
+                    }
+                    else if (purchaseMenuSelection==3)
+                    {
+                        //call finish transaction method
+                    }
+                    else
+                    {
+                        //if they enter in a wrong thing
+                    }
+                }
+                else if (mainMenuSelection==3)
+                {
+                    //Exit Program
+                }
+                else
+                {
+                    //if they enter in a wrong thing
+                }
+
             }
 
         }
