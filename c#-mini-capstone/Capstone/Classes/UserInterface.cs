76f1cedfd1d1de99598d23030a8c92e314ed2568
Rebekah Sherman <dirtbeans@gmail.com>
2018-06-14 15:19:47 -0400

    oh yeah, save button

--- c#-mini-capstone/Capstone/Classes/UserInterface.cs
+++ c#-mini-capstone/Capstone/Classes/UserInterface.cs
@@ -15,40 +15,51 @@ namespace Capstone.Classes
 
         }
 
+        public void MainMenu()
+        {
+            Console.WriteLine("{1} Display Vending Machine Items");
+            Console.WriteLine("{2} Purchase");
+            Console.WriteLine("{3} Exit");
+
+        }
+        public void PurchaseMenu()
+        {
+            Console.WriteLine("{1} Feed Money");
+            Console.WriteLine("{2} Select Product");
+            Console.WriteLine("[3] Finish Transation");
+           // Console.WriteLine($"Current Money Provided: ${Balance}");
+        }
+
         public void RunInterface()
         {
+
             bool done = false;
             while (!done)
             {
-                Console.WriteLine("{1} Display Vending Machine Items");
-                Console.WriteLine("{2} Purchase");
-                Console.WriteLine("[3] Exit");
+                MainMenu();
                 string mainMenuSelectionString = Console.ReadLine();
                 int mainMenuSelection = Convert.ToInt32(mainMenuSelectionString);
 
-                if ( mainMenuSelection == 1)
+                if (mainMenuSelection == 1)
                 {
                     //display Inventory
                     Console.WriteLine();
                 }
                 else if (mainMenuSelection == 2)
                 {
-                    Console.WriteLine("{1} Feed Money");
-                    Console.WriteLine("{2} Select Product");
-                    Console.WriteLine("[3] Finish Transation");
-                    // Console.WriteLine(balance);
+                    PurchaseMenu();
                     string purchaseMenuSelectionString = Console.ReadLine();
                     int purchaseMenuSelection = Convert.ToInt32(purchaseMenuSelectionString);
 
-                    if (purchaseMenuSelection==1)
+                    if (purchaseMenuSelection == 1)
                     {
                         //balance
                     }
-                    else if (purchaseMenuSelection==2)
+                    else if (purchaseMenuSelection == 2)
                     {
                         //call select product method
                     }
-                    else if (purchaseMenuSelection==3)
+                    else if (purchaseMenuSelection == 3)
                     {
                         //call finish transaction method
                     }
@@ -57,7 +68,7 @@ namespace Capstone.Classes
                         Console.WriteLine("Please enter a valid choice");
                     }
                 }
-                else if (mainMenuSelection==3)
+                else if (mainMenuSelection == 3)
                 {
                     //Exit Program
                     done = true;
