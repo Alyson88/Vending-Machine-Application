05c59ff106bc6b4b3b87a79535e0b3a58adc9c83
Rebekah Sherman <dirtbeans@gmail.com>
2018-06-14 23:21:23 -0400

    unit testing

---
+++ c#-mini-capstone/CapstoneTests/DispenseItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class DispenseItem
+    {
+     
+        [TestMethod]
+        public void Does_The_Item_Take_Away_The_Correct_Price()
+        {
+            //this seems wrong but I don't know how to fix if so
+            VendingMachine test = new VendingMachine();
+            test.Balance = 5;
+            test.DecreaseBalance(1);
+            Assert.AreEqual(4, test.Balance);
+
+        }
+    }
+}
---
+++ c#-mini-capstone/CapstoneTests/IncreaseBalance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class IncreaseBalance
+    {
+       
+        [TestMethod]
+        public void Does_The_Balance_Increase_With_A_Positive_Number()
+        {
+            VendingMachine test = new VendingMachine();
+            test.Balance = 5;
+            test.IncreaseBalance(5);
+            Assert.AreEqual(10, test.Balance);
+        }
+        [TestMethod]
+        public void Does_The_Balance_Increase_With_A_Negative_Number()
+        {
+            VendingMachine test = new VendingMachine();
+            test.Balance = 10;
+            test.IncreaseBalance(-5);
+            Assert.AreEqual(5, test.Balance);
+        }
+
+    }
+}
